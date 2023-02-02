a4be3d403a054d9fe04610b5a0f9f17ea02a6411
Salih Şenol Çakarcı <salihcakarci@gmail.com>
2023-02-02 18:52:41 +0100

    feat: support issue_comment (#33)
    * feat: support issue_comment
    
    * feat: checkout to the branch

---
+++ src/utils/generate-slack-message/generate-pull-request-comment-added-message.ts
@@ -0,0 +1,25 @@
+import {Context} from '@actions/github/lib/context'
+import {Block, KnownBlock} from '@slack/types'
+import {getUserToLog} from '../get-user-to-log'
+
+export const generatePullRequestCommentAddedMessage = (
+  githubContext: Context,
+  githubSlackUserMapper: Record<string, string>
+): (KnownBlock | Block)[] => {
+  const {comment, issue} = githubContext.payload
+  return [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Hi ${getUserToLog(
+          githubSlackUserMapper,
+          issue?.user.login
+        )}, a new <${comment?.html_url}|review comment> added by ${getUserToLog(
+          githubSlackUserMapper,
+          githubContext.actor
+        )}`
+      }
+    }
+  ]
+}
