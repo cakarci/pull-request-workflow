fdff04f357a510a7d509f03a33890724a670dfb6
Salih Şenol Çakarcı <salihcakarci@gmail.com>
2023-10-16 22:20:36 +0200

    fix: refactor pull-request-reminder (#58)
    * fix: refactor pull-request-reminder
    
    * chore: update dist folder

--- src/services/pull-request-reminder/pull-request-reminder.ts
+++ src/services/pull-request-reminder/pull-request-reminder.ts
@@ -1,5 +1,8 @@
 import {listPullRequestsParameters} from '../../api/github/list-pull-requests'
-import {githubService} from '../../api/github'
+import {
+  githubService,
+  OctokitListPullRequestsResponseType
+} from '../../api/github'
 import {
   generatePullRequestAuthorReminderMessage,
   generatePullRequestChangeRequesterReminderMessage,
@@ -15,6 +18,10 @@ interface PullRequestReminderParameters {
   githubSlackUserMapper: Record<string, string>
   remindAfter: number | undefined
 }
+
+/**
+ * Send reminders for pull requests.
+ */
 export const pullRequestReminder = async (
   {
     githubUserNames,
@@ -24,91 +31,176 @@ export const pullRequestReminder = async (
   {owner, repo, state = 'open'}: listPullRequestsParameters
 ): Promise<void> => {
   if (!remindAfter) {
+    // No need to send reminders if remindAfter is not defined.
     return
   }
-  const pulls = await githubService.listPullRequests({owner, repo, state})
+
+  const pulls: OctokitListPullRequestsResponseType['data'] =
+    await githubService.listPullRequests({owner, repo, state})
+
   if (pulls.length === 0) {
+    // No pull requests to remind about.
     return
   }
-  for (const {
-    number,
-    updated_at,
-    requested_reviewers,
-    user,
-    html_url
-  } of pulls) {
-    if (isTimeToRemind(updated_at, remindAfter)) {
-      const thread = await getPullRequestThread({
-        repoName: repo,
-        prNumber: number
-      })
 
-      if (!thread?.ts) {
-        return
+  for (const pullRequest of pulls) {
+    await processPullRequest(
+      pullRequest,
+      remindAfter,
+      githubUserNames,
+      githubSlackUserMapper,
+      owner,
+      repo
+    )
+  }
+}
+
+/**
+ * Check if it's time to send a reminder for a pull request.
+ */
+const isTimeToRemind = (updatedAt: string, remindAfter: number): boolean => {
+  const currentTime = new Date().getTime()
+  const updatedAtTime = new Date(updatedAt).getTime()
+  const remindThreshold = 3600000 * remindAfter // Convert hours to milliseconds.
+  return currentTime - remindThreshold > updatedAtTime
+}
+
+/**
+ * Process an individual pull request and send reminders if necessary.
+ */
+const processPullRequest = async (
+  pullRequest: OctokitListPullRequestsResponseType['data'][number],
+  remindAfter: number,
+  githubUserNames: string[],
+  githubSlackUserMapper: Record<string, string>,
+  owner: string,
+  repo: string
+): Promise<void> => {
+  const {number, updated_at, requested_reviewers, user, html_url} = pullRequest
+
+  if (isTimeToRemind(updated_at, remindAfter)) {
+    const thread = await getPullRequestThread({
+      repoName: repo,
+      prNumber: number
+    })
+
+    if (!thread?.ts) {
+      // Skip if there's no thread timestamp.
+      return
+    }
+
+    const reviewStateUsers = await getPullRequestReviewStateUsers(
+      {
+        prAuthor: user?.login as string,
+        requestedReviewers: requested_reviewers?.map(
+          (r: {login: string}) => r.login
+        ) as string[],
+        githubUserNames
+      },
+      {
+        owner,
+        repo,
+        pull_number: number
       }
+    )
 
-      const {APPROVED, CHANGES_REQUESTED, SECOND_APPROVERS} =
-        await getPullRequestReviewStateUsers(
-          {
-            prAuthor: user?.login as string,
-            requestedReviewers: requested_reviewers?.map(
-              (r: {login: string}) => r.login
-            ) as string[],
-            githubUserNames
-          },
-          {
-            owner,
-            repo,
-            pull_number: number
-          }
+    sendAuthorReminderIfApplicable(
+      reviewStateUsers.APPROVED.length,
+      reviewStateUsers.CHANGES_REQUESTED.length,
+      html_url,
+      thread.ts,
+      user?.login as string,
+      githubSlackUserMapper
+    )
+
+    sendReviewerRemindersIfApplicable(
+      reviewStateUsers.APPROVED.length,
+      reviewStateUsers.SECOND_APPROVERS,
+      html_url,
+      thread.ts,
+      githubSlackUserMapper
+    )
+
+    sendChangeRequesterRemindersIfApplicable(
+      reviewStateUsers.APPROVED.length,
+      reviewStateUsers.CHANGES_REQUESTED,
+      html_url,
+      thread.ts,
+      githubSlackUserMapper
+    )
+  }
+}
+
+/**
+ * Send a reminder message to the author of the pull request if applicable.
+ */
+const sendAuthorReminderIfApplicable = (
+  approvedCount: number,
+  changesRequestedCount: number,
+  htmlUrl: string,
+  threadTs: string,
+  authorLogin: string,
+  slackUserMapper: Record<string, string>
+): void => {
+  if (approvedCount === 2 && changesRequestedCount === 0) {
+    Slack.postMessage({
+      channel: core.getInput('slack-channel-id'),
+      thread_ts: threadTs,
+      blocks: generatePullRequestAuthorReminderMessage(
+        slackUserMapper,
+        authorLogin,
+        htmlUrl
+      )
+    })
+  }
+}
+
+/**
+ * Send reminders to additional reviewers if applicable.
+ */
+const sendReviewerRemindersIfApplicable = (
+  approvedCount: number,
+  secondApprovers: string[],
+  htmlUrl: string,
+  threadTs: string,
+  slackUserMapper: Record<string, string>
+): void => {
+  if (approvedCount <= 1 && secondApprovers.length !== 0) {
+    for (const secondApprover of secondApprovers) {
+      Slack.postMessage({
+        channel: core.getInput('slack-channel-id'),
+        thread_ts: threadTs,
+        blocks: generatePullRequestReviewerReminderMessage(
+          slackUserMapper,
+          secondApprover,
+          htmlUrl
         )
-
-      if (APPROVED.length === 2 && CHANGES_REQUESTED.length === 0) {
-        await Slack.postMessage({
-          channel: core.getInput('slack-channel-id'),
-          thread_ts: thread?.ts,
-          blocks: generatePullRequestAuthorReminderMessage(
-            githubSlackUserMapper,
-            user?.login as string,
-            html_url
-          )
-        })
-      }
-
-      if (APPROVED.length <= 1 && SECOND_APPROVERS.length !== 0) {
-        for (const secondApprover of SECOND_APPROVERS) {
-          await Slack.postMessage({
-            channel: core.getInput('slack-channel-id'),
-            thread_ts: thread?.ts,
-            blocks: generatePullRequestReviewerReminderMessage(
-              githubSlackUserMapper,
-              secondApprover,
-              html_url
-            )
-          })
-        }
-      }
-
-      if (APPROVED.length === 2 && CHANGES_REQUESTED.length !== 0) {
-        for (const changesRequester of CHANGES_REQUESTED) {
-          await Slack.postMessage({
-            channel: core.getInput('slack-channel-id'),
-            thread_ts: thread?.ts,
-            blocks: generatePullRequestChangeRequesterReminderMessage(
-              githubSlackUserMapper,
-              changesRequester,
-              html_url
-            )
-          })
-        }
-      }
+      })
     }
   }
 }
 
-const isTimeToRemind = (updatedAt: string, remindAfter: number): boolean => {
-  const today = new Date().getTime()
-  const updatedAtDate = new Date(updatedAt).getTime()
-  const hoursInMilliSecond = 3600000 * remindAfter
-  return today - hoursInMilliSecond > updatedAtDate
+/**
+ * Send reminders to change requesters if applicable.
+ */
+const sendChangeRequesterRemindersIfApplicable = (
+  approvedCount: number,
+  changesRequesters: string[],
+  htmlUrl: string,
+  threadTs: string,
+  slackUserMapper: Record<string, string>
+): void => {
+  if (approvedCount === 2 && changesRequesters.length !== 0) {
+    for (const changesRequester of changesRequesters) {
+      Slack.postMessage({
+        channel: core.getInput('slack-channel-id'),
+        thread_ts: threadTs,
+        blocks: generatePullRequestChangeRequesterReminderMessage(
+          slackUserMapper,
+          changesRequester,
+          htmlUrl
+        )
+      })
+    }
+  }
 }
